05e14cb75822987424a653963c94deaef7872f8f
Pavlo Krysenko <pavlo.krysenko@wolterskluwer.com>
2018-10-16 11:26:39 +0300

    LinkedList Implemented

---
+++ ListLinked/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList
+{
+    public class Node<T>
+    {
+        public T _value { get; set; }
+        public Node<T> next { get; set; }
+        public Node(T value)
+        {
+            _value = value;
+        }
+
+    }
+}
